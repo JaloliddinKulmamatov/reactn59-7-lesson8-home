09008cded9939d2d76580c3f7fb9682b3cd09e7d
JaloliddinKulmamatov <itjalol2010@gmail.com>
2024-07-08 07:01:24 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,124 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import { Routes, useNavigate, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+import axios from "axios";
+import api from "./api/posts";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [newPostLoading, setNewPostLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          api
+        );
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Failed to fetch posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    setNewPostLoading(true);
+    try {
+      const response = await axios.post(
+        api,
+        newPost,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const savedPost = response.data;
+      const allPosts = [...posts, savedPost];
+      setPosts(allPosts);
+      setPostTitle("");
+      setPostBody("");
+      navigate("/");
+    } catch (error) {
+      console.error("Failed to save the post:", error);
+    } finally {
+      setNewPostLoading(false);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(
+        `${api}/${id}`
+      );
+
+      const postsList = posts.filter((post) => post.id !== id);
+      setPosts(postsList);
+      navigate("/");
+    } catch (error) {
+      console.error("Failed to delete the post:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
+      <Routes>
+        <Route path="/" element={<Home posts={searchResults} />} />
+        <Route
+          path="/post"
+          element={
+            <NewPost
+              handleSubmit={handleSubmit}
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+              loading={newPostLoading}
+            />
+          }
+        />
+        <Route
+          path="/post/:id"
+          element={<PostPage posts={posts} handleDelete={handleDelete} />}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api/posts.js
@@ -0,0 +1,3 @@
+// src/api/posts.js
+const api = "https://reactn59-7-lesson-7-posts-api-3.onrender.com/posts";
+export default api;
